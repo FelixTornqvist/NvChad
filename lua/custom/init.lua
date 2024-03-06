ce92a9442f0438baa30837e09ea5a6fd3845573e
Felix Törnqvist <felix.tornqvist@bitio.se>
2024-03-06 16:40:04 +0100

    windows settings

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -12,19 +12,14 @@ autocmd("BufWritePre", {
   command = "lua require('conform').format()",
 })
 
--- set default terminal to git bash
--- vim.api.nvim_set_var("terminal_emulator", "bash.exe")
--- vim.o.shell = 'C:\\"Program Files"\\Git\\bin\\bash.exe'
-
--- set shellcmdflag=-c to run bash commands
--- vim.o.shellcmdflag = '--rcfile "C:\\Users\\Felix Törnqvist\\.bashrc" -c'
--- vim.o.shellcmdflag = "-c"
-
 -- set relativenumber
 vim.wo.relativenumber = true
 
--- set diff highlighting to be more visible
--- vim.cmd "highlight DiffAdd cterm=bold ctermfg=10 ctermbg=NONE"
--- vim.cmd "highlight DiffChange cterm=bold ctermfg=11 ctermbg=NONE"
--- vim.cmd "highlight DiffDelete cterm=bold ctermfg=9 ctermbg=NONE"
--- vim.cmd "highlight DiffText cterm=bold ctermfg=12 ctermbg=NONE"
+-- Windows specific settings
+
+if vim.fn.has "win32" == 1 then
+  -- set shell to git bash
+  vim.o.shell = 'C:\\"Program Files"\\Git\\bin\\bash.exe'
+  -- set shellcmdflag=-c to run bash commands
+  vim.o.shellcmdflag = "-c"
+end

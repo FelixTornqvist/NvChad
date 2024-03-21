a3c6e14ecec220dee3af65efc109b286353078a2
Felix Törnqvist <felix.tornqvist@bitio.se>
2024-03-21 12:13:00 +0100

    disable bracket auto-pairs

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -76,6 +76,11 @@ local plugins = {
   --   enabled = false
   -- },
 
+  {
+    "windwp/nvim-autopairs",
+    enabled = false,
+  },
+
   -- All NvChad plugins are lazy-loaded by default
   -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
   -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
